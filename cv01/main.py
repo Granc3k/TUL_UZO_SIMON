02297f22161383081344f75d3c528255bc0b81df
Martin Granc3k Šimon - PRDOLA <mart.simon17@gmail.com>
2025-02-12 13:23:22 +0100

    cv01 done & submitted

--- cv01/main.py
+++ cv01/main.py
@@ -6,6 +6,7 @@ import os
 
 
 def load_images_from_folder(folder):
+    # Load obrázků z folderu
     images = {}
     for filename in os.listdir(folder):
         img_path = os.path.join(folder, filename)
@@ -16,30 +17,35 @@ def load_images_from_folder(folder):
 
 
 def compute_histogram(image):
+    # Výpočet histogramu z grayscalu
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+    # normalizace histogramu
     cv2.normalize(hist, hist)
     return hist
 
 
 def compare_histograms(hist1, hist2):
-    return cv2.compareHist(hist1,hist2, cv2.HISTCMP_BHATTACHARYYA)
+    # porovnání 2 histogramů pomocí bhattacharyya vzdálenosti
+    return cv2.compareHist(hist1, hist2, cv2.HISTCMP_BHATTACHARYYA)
 
 
 def sort_images(images, reference_image):
+    # ref histogram
     reference_hist = compute_histogram(reference_image)
     distances = []
-
+    # porovnání všech obrázků s referenčním
     for name, img in images.items():
         hist = compute_histogram(img)
         distance = compare_histograms(reference_hist, hist)
         distances.append((name, distance, img))
-
-    distances.sort(key=lambda x: x[1])  # Sort by similarity
+    # sort přes lambdu
+    distances.sort(key=lambda x: x[1])
     return distances
 
 
 def display_sorted_images_grid(images):
+
     image_names = list(images.keys())
     num_images = len(image_names)
     fig, axes = plt.subplots(num_images, num_images, figsize=(15, 15))
@@ -48,17 +54,18 @@ def display_sorted_images_grid(images):
         reference_image = images[ref_name]
         sorted_images = sort_images(images, reference_image)
 
-        for j, (name, _, img) in enumerate(sorted_images):
+        for j, (name, score, img) in enumerate(sorted_images):
             ax = axes[i, j]
             ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-            ax.set_title(name)
+            # Print score histogramu pro validaci
+            ax.set_title(f"Score: {score:.4f}")
             ax.axis("off")
 
     plt.tight_layout()
     plt.show()
 
 
-# Main execution
+# Main fce
 def main():
     folder_path = "./data/"
     images = load_images_from_folder(folder_path)
@@ -66,7 +73,7 @@ def main():
     if images:
         display_sorted_images_grid(images)
     else:
-        print("No images found in the folder.")
+        print("Nic to nenalezlo")
 
 
 if __name__ == "__main__":
